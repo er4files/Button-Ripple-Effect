5a2ce2d7b0d506f63a3eb126224fe53d5fac0aac
Rahmad Era Sugiarto <rahmaderasugiarto@gmail.com>
2023-12-12 20:58:21 +0700

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn1 = document.querySelector(".btn");
+
+btnE1.addEventListener("mouseover", (event) => {
+    const x =event.pageX - btnE1.offsetLeft;
+    const y =event.pageY - btnE1.offsetTop;
+})
